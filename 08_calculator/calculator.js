213cd8cb1e3ef2884d2be42ba50e2d541c35d029
Rafael-Correzi <112991986+Rafael-Correzi@users.noreply.github.com>
2024-03-28 15:58:40 -0300

    Finish exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,58 @@
-const add = function() {
+const add = function(...numbersSummed) {
+  let sum = 0;
+  for (let n of numbersSummed){
+    sum += n;
+  }
+  return sum;
 	
 };
 
-const subtract = function() {
+const subtract = function(...numbersSubtracted) {
+  let subtraction = numbersSubtracted[0]*2;
+  for (let n of numbersSubtracted){
+    subtraction -= n;
+  }
+  return subtraction
 	
 };
 
-const sum = function() {
+const sum = function(arr) {
+  let sum = 0;
+  for (let n of arr){
+    sum += n;
+  }
+  return sum;
 	
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let multiplication = 1;
+  for (let n of arr) {
+    multiplication *= n;
+  }
+  return multiplication;
 
 };
 
-const power = function() {
+const power = function(base, exponent) {
+  let result = 1;
+  for (i = 1; i <= exponent; i++) {
+    result *= base;
+  }
+  return result;
 	
 };
 
-const factorial = function() {
+const factorial = function(number) {
+  if (number < 0) {
+    return('ERROR');
+  }
+  let result = 1;
+  for(i = number; i > 0; i--) {
+    result *= i;
+  }
+  return result;
+    
 	
 };
 

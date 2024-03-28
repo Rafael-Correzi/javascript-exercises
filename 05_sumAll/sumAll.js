209b4e7c9455b6723c5ba1ca244089cbcabceaea
Rafael-Correzi <112991986+Rafael-Correzi@users.noreply.github.com>
2024-03-28 00:35:32 -0300

    Finsih exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+  let sum = 0;
+  if (typeof (number1) != "number") {
+    return('ERROR')
+  }
+  if (typeof (number2) != "number") {
+    return('ERROR')
+  }
+  if (number1 < 0 || number2< 0) {
+    return('ERROR');
+  }
+  for (i = number1; i <= number2; i++) {
+    sum += i;
+  }
+  for (i = number2; i <= number1; i++) {
+    sum += i;
+  }
+  return sum;
 
 };
 

2735970c308a05037c6ba7219cc86d0b0ca4fb69
Rafael-Correzi <112991986+Rafael-Correzi@users.noreply.github.com>
2024-10-02 14:55:50 -0300

    complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+  let temp;
+  let fiboPrevious = 0;
+  let fiboCurrent = 1;
+  if (number==0) {
+    return 0;
+  }
+  if (number < 0 ) {
+    return "OOPS";
+  }
+  for (i=1; i<number; i++) {
+    temp = fiboCurrent;
+    fiboCurrent += fiboPrevious;
+    fiboPrevious = temp;
+  }
+  return fiboCurrent;
 };
 
 // Do not edit below this line

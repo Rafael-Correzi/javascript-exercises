7ec26b88cff5981d5d525b18c50c882a49d67c66
Rafael-Correzi <112991986+Rafael-Correzi@users.noreply.github.com>
2024-10-02 16:21:40 -0300

    Complete findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  const sortedPeople = people.sort(function(a , b) {
+    const currentYear = new Date().getFullYear();
+    const aYearFinal = a.yearOfDeath == undefined ? currentYear : a.yearOfDeath;
+    const bYearFinal = b.yearOfDeath == undefined ? currentYear : b.yearOfDeath;
+  
+    const thisPerson = aYearFinal- a.yearOfBirth;
+    const nextPerson = bYearFinal - b.yearOfBirth;
+    return thisPerson > nextPerson ? -1 : 1;
+  });
+  return sortedPeople[0];
 };
 
 // Do not edit below this line
